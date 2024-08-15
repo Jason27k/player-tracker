b508cdd8ab7756d1e7b385abe4f8de40cce1c4ba
Jason27k <68726620+Jason27k@users.noreply.github.com>
2024-08-15 09:51:51 -0700

    Initial Commit

---
+++ us-tracker/us_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  us_trackerApp.swift
+//  us-tracker
+//
+//  Created by Jason Morales on 8/15/24.
+//
+
+import SwiftUI
+
+@main
+struct us_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
